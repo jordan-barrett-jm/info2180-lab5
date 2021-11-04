b7e6083972b7b1ee0b92b8b9d2bcd666d2da804a
unknown <jordan.barrett.jm@gmail.com>
2021-11-03 20:44:41 -0500

    Exercise 1 completed

---
+++ world.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+    var searchBtn = document.getElementById("lookup");
+    searchBtn.onclick = countrySearch;
+    console.log(searchBtn.onclick);
+}
+
+function countrySearch (e){
+    var searchString = document.getElementById("country").value;
+    console.log(country);
+    var req = new Request('world.php');
+    if (searchString.length > 0){
+        //sanitize
+        searchString = searchString.replaceAll(">","").replaceAll("<", "") ;
+        req = new Request(`world.php?country=${searchString}`);
+    }
+    const myInit = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+        };
+    fetch(req, myInit).then( res => {
+        res.text().then(data => {
+            resDiv = document.getElementById("result");
+            resDiv.innerHTML = data;
+        });
+    });
+}
